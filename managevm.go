dc318b5c7f94534a9337f350732da9b58b9b1e75
José Carvalho <jcarvalhoprojects@gmail.com>
2014-05-03 14:32:02 +0200

    included list and destroy

--- managevm.go
+++ managevm.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	app := cli.NewApp()
 
-	var start = cli.Command{
+	start := cli.Command{
 		Name:        "start",
 		ShortName:   "s",
 		Usage:       "Initializes the desired vm in headless mode",
@@ -43,8 +43,94 @@ func main() {
 		},
 	}
 
+	list := cli.Command{
+		Name:        "list",
+		ShortName:   "l",
+		Usage:       "Display currently running or existing vms",
+		Description: "Shows detail about currently running or existing vms",
+		Subcommands: []cli.Command{
+			{
+				Name:  "available",
+				Usage: "Shows available vms",
+				Action: func(c *cli.Context) {
+					binary, err := exec.LookPath("VBoxManage")
+
+					if err != nil {
+						panic(err)
+					}
+
+					args := []string{"VBoxManage", "list", "vms"}
+
+					env := os.Environ()
+
+					err = syscall.Exec(binary, args, env)
+
+					if err != nil {
+						panic(err)
+					}
+				},
+			},
+			{
+				Name:  "running",
+				Usage: "Shows currently running vms",
+				Action: func(c *cli.Context) {
+					binary, err := exec.LookPath("VBoxManage")
+
+					if err != nil {
+						panic(err)
+					}
+
+					args := []string{"VBoxManage", "list", "runningvms"}
+
+					env := os.Environ()
+
+					err = syscall.Exec(binary, args, env)
+
+					if err != nil {
+						panic(err)
+					}
+				},
+			},
+		},
+		Action: cli.ShowSubcommandHelp,
+	}
+
+	destroy := cli.Command{
+		Name:        "destroy",
+		ShortName:   "d",
+		Usage:       "Kills a currently running vm",
+		Description: "Expects the vm name as the argument.",
+		Action: func(c *cli.Context) {
+
+			vm_name := c.Args().First()
+
+			if vm_name == "" {
+				println("Please specify vm name")
+				return
+			}
+
+			binary, err := exec.LookPath("VBoxManage")
+
+			if err != nil {
+				panic(err)
+			}
+
+			args := []string{"VBoxManage", "controlvm", vm_name, "poweroff"}
+
+			env := os.Environ()
+
+			err = syscall.Exec(binary, args, env)
+
+			if err != nil {
+				panic(err)
+			}
+		},
+	}
+
 	app.Commands = []cli.Command{
 		start,
+		list,
+		destroy,
 	}
 
 	app.Version = "0.0.1"
